d9d11deda44a71cc16c5168c59bbf5ec8412d830
Arkwings <henriques.nicolas@hotmail.fr>
2022-06-03 14:32:08 +0200

    feat: othello.hpp

---
+++ othello.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+Othello
+{
+public:
+        Othello(const std::string& player_one, const std::string& player_two, const int& idle_time)
+        : player_one_(player_one),
+        , player_two_(player_two),
+        , idle_time_(idle_time)
+        {}
+
+        ~Othello()
+        {}
+
+
+private:
+        const std::string player_one_;
+        const std::string player_two_;
+        const int idle_time_;
+};
